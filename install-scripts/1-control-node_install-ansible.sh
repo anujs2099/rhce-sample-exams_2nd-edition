b5863f087eef91a8129d5c249d8189fd78d1f1d8
student <student@control.example.com>
2023-07-23 12:13:02 -0400

    updated files

--- install-scripts/1-control-node_install-ansible.sh
+++ install-scripts/1-control-node_install-ansible.sh
@@ -2,13 +2,13 @@
 
 #subscription-manager repos --list
 echo ""
-subscription-manager repos --enable rhel-9-for-x86_64-appstream.rpms
+subscription-manager repos --enable rhel-9-for-x86_64-appstream-rpms
 
 echo ""
 subscription-manager repos --list | grep ansible-2-for-rhel-8-x86_64-rpms -A3
 
 echo ""
-subscription-manager repos --enable ansible-automation-platform-2.2-for-rhel-9-x86_64-rpms
+subscription-manager repos --enable ansible-automation-platform-2.4-for-rhel-9-x86_64-rpms
 
 echo ""
 yum repolist
